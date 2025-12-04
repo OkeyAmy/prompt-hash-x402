0afb495dd8eb9fe774a38e4c136168cf5a775288
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2025-12-04 14:23:19 +0100

    fix: update types for node and react, pinning versions for stability

--- api/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from api.routers.model import router as models_router
-from api.routers.chat import router as chat_router
-from api.routers.prompt_improver import router as prompt_improver_router
-from api.routers.health import router as health_router
